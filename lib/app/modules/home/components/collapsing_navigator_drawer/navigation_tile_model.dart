a682b5ff882226cecc5239b279c8eac6894b3981
Engenharia <jhsilva149@gmail.com>
2020-07-26 19:27:47 -0300

    Custom Drawer commit fix

--- lib/app/modules/home/components/collapsing_navigator_drawer/navigation_tile_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavigationTileModel {
-  String title;
-  IconData icon;
-  String routeMenu;
-
-  NavigationTileModel({this.title, this.icon, this.routeMenu});
-}
